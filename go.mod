aa82289632f7ee9cdfa0116fcdb0dd77a2843877
letsfire <249008728@qq.com>
2020-03-11 18:02:00 +0800

    初版完成

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/easy-bus/redis-idempotent
+
+go 1.12
+
+require (
+	github.com/easy-bus/bus v0.0.0-20200310024416-638b62107f74
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/letsfire/redigo v1.0.1
+	github.com/letsfire/utils v0.0.0-20200228103629-1fb949eabd43 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ redis.go
@@ -0,0 +1,28 @@
+package redis_idempotent
+
+import (
+	"github.com/easy-bus/bus"
+	"github.com/gomodule/redigo/redis"
+	"github.com/letsfire/redigo"
+)
+
+type redisIdempotent struct {
+	client *redigo.Client
+}
+
+func (ri redisIdempotent) Acquire(key string) (bool, error) {
+	return ri.client.Bool(func(conn redis.Conn) (interface{}, error) {
+		return conn.Do("SETNX", key, 1)
+	})
+}
+
+func (ri redisIdempotent) Release(key string) error {
+	_, err := ri.client.Execute(func(conn redis.Conn) (interface{}, error) {
+		return conn.Do("DEL", key)
+	})
+	return err
+}
+
+func New(client *redigo.Client) bus.IdempotentInterface {
+	return &redisIdempotent{client: client}
+}
